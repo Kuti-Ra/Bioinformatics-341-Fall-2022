77b4934ae99cac70c4e7b6fa55ff9ffd2485f332
Kuti-Ra <111650206+Kuti-Ra@users.noreply.github.com>
2022-08-29 09:45:33 -0400

    Add files via upload

---
+++ First-R-Program.R
@@ -0,0 +1,10 @@
+#Author: Ra; Date: 08/29/2022; Purpose: Testing R script
+
+#Install microarray packages
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("affy")
+
+#Load the package affy
+librayr(affy)
