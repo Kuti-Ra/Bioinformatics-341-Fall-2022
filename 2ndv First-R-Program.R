3abef4ba147bec2dba557618c2796b7f3f9ca98c
Kuti-Ra <111650206+Kuti-Ra@users.noreply.github.com>
2022-09-02 09:55:11 -0400

    Add files via upload

---
+++ 2ndv First-R-Program.R
@@ -0,0 +1,39 @@
+#Author: Ra; Date: 08/29/2022; Purpose: Testing R script
+
+#Install microarray packages
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("affy")
+
+#Load the package affy
+library(affy)
+
+# Read CEL microarray files in R using library affy
+
+# Setting the working directory
+setwd("/Users/crypt/Music/r working directory")
+
+#Read the CEL files from current working directory 
+eset <- ReadAffy()
+
+#Extracting expression values using following function
+rma <- exprs(eset)
+
+#Boxplot to view the expression of samples
+boxplot(rma)
+
+#Pre-processing/Normalization of the chips: Mas5, RMA, Quantile Normalization
+
+#Applying RNA normalization function
+
+#Install package oligo
+if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager");BiocManager::install("oligo")
+
+# Applying RMA normalizaton function
+library(oligo)
+celFiles = list.celfiles()
+affyRaw <- read.celfiles(celFiles)
+eset <- rma(affyRaw)
+boxplot(eset)
+
